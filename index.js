d0e9e477901c909959861bbb402c348d85ff33e1
Eric Farris <efarris1993@gmail.com>
2023-06-04 16:14:36 -0400

    Changes needed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Eric",
+    streetAddress: "123 Valley Avenue",
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee 
+        [key] value};
+        
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee [key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {...employee};
+    delete newEmployee [key];
+    return newEmployee;
+}
+
+function destructivleyDeleteFromEmployeeByKey(employee, key){
+    delete employee [key];
+    return employee;
+}
